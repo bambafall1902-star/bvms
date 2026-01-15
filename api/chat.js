cc7aa36d335cdf7a9ec67c5048ad5a4dc3c5e608
bambafall1902-star <bambafall1902@gmail.com>
2026-01-15 23:49:07 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,11 @@
 export default function handler() {
   return new Response(
-    JSON.stringify
+    JSON.stringify({
+      message: "API OK - le serveur fonctionne"
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    }
+  );
+}
